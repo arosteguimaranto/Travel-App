7b57a1fa73e2500fc64a96f349ae907cb7ce4808
arosteguimaranto <arosteguimaranto@outlook.com>
2023-12-05 20:22:33 -0600

    feat:  add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      footer
+    <footer className='flexCenter mb-24'>
+      <div className='padding-container max-container flex w-full flex-col gap-14'>
+    <div className='flex flex-col items-start justify-center gap-[10%] md:flex-row'>
+    
     </div>
+      </div>
+    </footer>
   )
 }
 
